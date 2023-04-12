ba69b1406e63e710939b9c50081488ce82c5b910
theopmw <theowrightdev@gmail.com>
2023-04-12 09:00:29 +0100

    Add http.js test

---
+++ util/http.spec.js
@@ -0,0 +1,25 @@
+import { describe, it, expect, vi } from 'vitest';
+import { sendDataRequest } from './http';
+
+const testResponseData = { testKey: 'testData' };
+const testFetch = vi.fn((url, options) => {
+  return new Promise((resolve, reject) => {
+    const testResponse = {
+      ok: true,
+      json() {
+        return new Promise((resolve, reject) => {
+          resolve(testResponseData);
+        });
+      },
+    };
+    resolve(testResponse);
+  });
+});
+
+vi.stubGlobal('fetch', testFetch);
+
+it('should return any available repsones data', () => {
+  const testData = { key: 'test' };
+
+  return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
