0b49e47953f3cc23c8b78f29dff0a9325385fca4
theopmw <theowrightdev@gmail.com>
2023-04-12 08:35:17 +0100

    Add validation.js tests

---
+++ util/vaildation.spec.js
@@ -0,0 +1,27 @@
+import { describe, it, expect } from 'vitest';
+import { validateNotEmpty } from './validation';
+
+it('should throw an error if an empty string is provided as a value', () => {
+  const testInput = '';
+
+  const validationFn = () => validateNotEmpty(testInput);
+
+  expect(validationFn).toThrow();
+});
+
+it('should throw an error if a non empty string is provided that only consists of blanks', () => {
+  const testInput = '  ';
+
+  const validationFn = () => validateNotEmpty(testInput);
+
+  expect(validationFn).toThrow();
+});
+
+it('should throw an error with the provided error message', () => {
+  const testInput = '';
+  const testErrorMessage = 'Error message';
+
+  const validationFn = () => validateNotEmpty(testInput, testErrorMessage);
+
+  expect(validationFn).toThrow(testErrorMessage);
+});
