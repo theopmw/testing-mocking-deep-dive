f367687bd43c0cc20862eca5f63bc9c2efa6ae59
theopmw <theowrightdev@gmail.com>
2023-04-12 08:23:23 +0100

    Add tests for errors.js

--- util/errors.spec.js
+++ util/errors.spec.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { HttpError } from './errors';
+import { HttpError, ValidationError } from './errors';
 
 describe('class HttpError', () => {
   it('should contain the provided status code, message and data', () => {
@@ -12,5 +12,32 @@ describe('class HttpError', () => {
     expect(testError.statusCode).toBe(testStatus);
     expect(testError.message).toBe(testMessage);
     expect(testError.data).toBe(testData);
+  });
+
+  it('should contain undefined as data if no data is provided', () => {
+    const testStatus = 1;
+    const testMessage = 'Test message';
+
+    const testError = new HttpError(testStatus, testMessage);
+
+    expect(testError.statusCode).toBe(testStatus);
+    expect(testError.message).toBe(testMessage);
+    expect(testError.data).not.toBeDefined();
+  });
+});
+
+describe('class validationError', () => {
+  it('should contain the provided message', () => {
+    const testMessage = 'Test message';
+
+    const testError = new ValidationError(testMessage);
+
+    expect(testError.message).toBe(testMessage);
+  });
+
+  it('should contain undefined as message if no message is provided', () => {
+    const testError = new ValidationError();
+
+    expect(testError.data).not.toBeDefined();
   });
 });
