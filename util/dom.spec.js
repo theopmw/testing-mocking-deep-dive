cfbf4cd028fb54e744689a5e4ab376b499a7cf40
theopmw <theowrightdev@gmail.com>
2023-04-12 15:06:09 +0100

    Set up virtual HTML page

--- util/dom.spec.js
+++ util/dom.spec.js
@@ -1,5 +1,18 @@
-import { describe, it, expect } from 'vitest';
+import fs from 'fs';
+import path from 'path';
+
+import { describe, it, expect, vi } from 'vitest';
+import { Window } from 'happy-dom';
+
 import { showError } from './dom';
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+document.write(htmlDocumentContent);
+vi.stubGlobal('document', document);
 
 it('first test', () => {
   showError('test');
